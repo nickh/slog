fa38eec09e8d6e06a154d306439dd1870131bfb7
Nick Hengeveld <nickh@hengeveld.com>
2008-08-16 15:54:47 -0700

    Switch from cookie to ActiveRecord sessions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080811151419) do
+ActiveRecord::Schema.define(:version => 20080816044228) do
 
   create_table "log_entries", :force => true do |t|
     t.datetime "created_at"
@@ -21,6 +21,23 @@ ActiveRecord::Schema.define(:version => 20080811151419) do
     t.integer  "days_onboard"
     t.integer  "night_hours"
     t.text     "notes"
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+
+  create_table "users", :force => true do |t|
+    t.string "openid_identifier"
+    t.string "nickname"
+    t.string "fullname"
+    t.string "email"
   end
 
 end
