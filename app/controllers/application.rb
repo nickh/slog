2b0a82e89a21c68a742ead64db77779bebe73ec0
Nick Hengeveld <nickh@hengeveld.com>
2008-08-25 23:00:10 -0700

    Clean up application controller
    - Remove scaffold/template comments
    - Remember that we need to add session expiration

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -10,14 +10,10 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => 'edc9493c014d3d7699ace5d4f8c1c703'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
-
   private
 
   def check_login
+    # TODO: session expiration
     @current_user = session[:user]
   end
 end
