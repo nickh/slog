4f0569eec2deb97df24d834499a98ef01e567cf4
Nick Hengeveld <nickh@hengeveld.com>
2008-08-25 23:01:22 -0700

    Clean up log entries migration

--- db/migrate/20080811151419_create_log_entries.rb
+++ db/migrate/20080811151419_create_log_entries.rb
@@ -9,16 +9,6 @@ class CreateLogEntries < ActiveRecord::Migration
     end
   end
 
-=begin
-@entry.should.respond_to?(:departed_at)
-@entry.should.respond_to?(:arrived_at)
-@entry.should.respond_to?(:origin)
-@entry.should.respond_to?(:destination)
-@entry.should.respond_to?(:days_onboard)
-@entry.should.respond_to?(:night_hours)
-@entry.should.respond_to?(:notes)
-=end
-
   def self.down
     drop_table :log_entries
   end
